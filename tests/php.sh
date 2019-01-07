574dc64d5543afa51983f8b77ff23f29c2e32903
Dan Hunsaker <danhunsaker@gmail.com>
2019-01-07 10:02:29 -0700

    Move PHP into separate stage

--- tests/php.sh
+++ tests/php.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
-start=$(dirname $(readlink -f $0))
+start="$(dirname "$0")"
+if [[ ${start:0:1} != '/' ]]
+then
+  start="$(dirname "$(pwd)/$0")"
+fi
 set -e
 
 # Ensure everything is built, first, so we're testing the latest code
+cd "$(dirname "${start}")"
+go get -v -t ./...
 cd libcalends
 go build -v -o libcalends.so -buildmode=c-shared .
 
 cd php
 zephir fullclean
-zephir builddev -v || (cat compile-errors.log; false)
+zephir build --dev -v || (cat compile-errors.log; false)
 
 # Run the actual tests
 cd ext
-TRAVIS='' NO_INTERACTION=true php run-tests.php -P -d "extension=calends.so" || ${start}/failed_test_info.sh
+TRAVIS='' NO_INTERACTION=true php run-tests.php -P -d "extension=calends.so" || true
+"${start}/failed_test_info.sh"
